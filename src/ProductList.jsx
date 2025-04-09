a99fcb2ae869373c14157867f3d7f952cd6137c8
BlueRana <bluefrog79sl@gmail.com>
2025-04-09 16:53:21 -0400

    working on css

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,7 +9,7 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({}); // State to track which products are added to cart
     const dispatch = useDispatch();
-    const cartItems = useSelector((state) => state.cart.items);
+    const cartItems = useSelector((state) => state.cart.items); // Get cart items from Redux store
 
     const plantsArray = [
         {
@@ -231,7 +231,7 @@ function ProductList({ onHomeClick }) {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
-        width: '1100px',
+        width: '52%',
     }
     const styleA = {
         color: 'white',
@@ -303,7 +303,13 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                            <rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle>
+                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" 
+                            fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                            <div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
+                            {/* Added <dic className=''>{totalQuantity} to show how many items are in cart */}
                 </div>
             </div>
             {!showCart ? (
@@ -317,11 +323,14 @@ function ProductList({ onHomeClick }) {
                                     <img className="product-image" src={plant.image} alt={plant.name} />
                                     <div className="product-title">{plant.name}</div>
                                     <div className='product-description'>{plant.description}</div>
-                                    <div className='product-cost'>{plant.cost}</div>
+                                    <div className='product-price'>{plant.cost}</div>
                                     {/*Similarly like the above plant.name show other details like description and cost*/}
                                     <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}
                                         onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.name]}
 >                                       {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}</button>
+                                            {/* ${...} Template literal addedTpCart[plant.name] access value ? (ternary operator)
+                                            If addedToCart[plant.name] is truthy (meaning the plant is in the cart), it returns the string 'added-to-cart'.
+                                            Otherwise (if the plant is not in the cart), it returns an empty string ('').*/}
                                 </div>
                                 ))}
                             </div>
